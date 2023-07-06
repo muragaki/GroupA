e89c2c822009da7395ac4b710411f7d20595c6f0
3030673 <3030673@30306-73>
2023-07-06 11:53:53 +0900

    matuo

--- src/main/java/com/example/librarySystem/app/admin/user/AdminUserController.java
+++ src/main/java/com/example/librarySystem/app/admin/user/AdminUserController.java
@@ -1,5 +1,53 @@
 package com.example.librarySystem.app.admin.user;
 
-public class AdminUserController {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.example.librarySystem.domain.model.LendLog;
+import com.example.librarySystem.domain.model.Lending;
+import com.example.librarySystem.domain.model.User;
+import com.example.librarySystem.domain.service.LendLogService;
+import com.example.librarySystem.domain.service.LendingService;
+import com.example.librarySystem.domain.service.SuperUserDetailsService;
+
+@Controller
+public class AdminUserController {
+	
+	@Autowired
+	SuperUserDetailsService superUserDetailsService;
+	
+	@Autowired
+	LendingService lendingService;
+	
+	@Autowired
+	LendLogService lendLogService;
+	
+	
+	@RequestMapping("admin/user")
+	String userlist(Model model) {
+		List<User> userlist = superUserDetailsService.getUserAll();
+		model.addAttribute("userlist", userlist);
+		return "admin/user/userlist";
+	}
+	
+	
+	@RequestMapping("admin/user/userdetail")
+	String userdetail(@RequestParam String userId, Model model) {
+		List<LendLog> lendLoglist = lendLogService.findUserList(userId);
+		List<Lending> lendinglist = lendingService.findUserList(userId);
+		User user = superUserDetailsService.findById(userId);
+		
+		model.addAttribute("lendLoglist", lendLoglist);
+		model.addAttribute("lendinglist", lendinglist);
+		model.addAttribute("u", user);
+		return "admin/user/userdetail";
+		
+	}
+
+	
 }
--- src/main/java/com/example/librarySystem/app/signup/SignupForm.java
+++ src/main/java/com/example/librarySystem/app/signup/SignupForm.java
@@ -1,5 +1,6 @@
 package com.example.librarySystem.app.signup;
 
+import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
 
@@ -7,8 +8,10 @@ import lombok.Data;
 @Data
 public class SignupForm {
 	
+	@Pattern(regexp="[a-zA-Z0-9]*")
 	@Size(min = 4 ,max = 4)
 	private String userId;
+	@Pattern(regexp="[a-zA-Z0-9]*")
 	@Size(min = 4 , max = 6)
 	private String password;
 	@Size(min = 1 , max = 10)
