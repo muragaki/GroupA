3e757a1635cb67c38abdd77113ee710c3aec9287
3030674 <3030674@30306-74>
2023-07-13 14:04:52 +0900

    nakao

--- src/main/java/com/example/librarySystem/domain/model/SituationName.java
+++ src/main/java/com/example/librarySystem/domain/model/SituationName.java
@@ -8,9 +8,9 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum SituationName implements Values {
 
-	AVAILABLE("AVAILABLE", "AVAILABLE"),					//利用可能
-	LENDING("LENDING", "LENDING"),							//貸出中
-	NOT_AVAILABELE("NOT_AVAILABLE", "NOT_AVAILABLE");		//貸出不可
+	AVAILABLE("AVAILABLE", "利用可能"),					//利用可能
+	LENDING("LENDING", "貸出中"),							//貸出中
+	NOT_AVAILABLE("NOT_AVAILABLE", "利用不可");		//貸出不可
 	
 
 	private final String value;
